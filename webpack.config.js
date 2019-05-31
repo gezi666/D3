1eeeaed415aee1456da8712feb9794fa379e6f80
gezi666 <3129168699@qq.com>
2019-05-31 14:10:08 +0800

    修改配置文件、重新打包

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ const webpack = require('webpack')
 const path = require("path")
 
 module.exports = {
+	mode: "development",
 	entry: "./src/index.js",
 	devtool: 'inline-source-map',
 	devServer: {
